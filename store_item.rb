9286b6be99246a2589895e15230e27ae3b926349
Ryan Searls <Ryan.searls@gmail.com>
2021-06-09 13:42:03 -0500

    store items as hashes

---
+++ store_item.rb
@@ -0,0 +1,13 @@
+item1 = {food: "banana", color: "yellow", price: 1.5}
+item2 = {food: "mango", color: "orange", price: 2.5}
+item3 = {"food" => "kiwi", "color" => "greeen", "price" => 3.0}
+
+puts "Welcome to my store. "
+
+
+
+
+class Store
+def Initialize()
+
+end  
