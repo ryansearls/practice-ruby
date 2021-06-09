189ceffc5f3e30859b7fa34989da048677e8c464
Ryan Searls <Ryan.searls@gmail.com>
2021-06-09 14:33:19 -0500

    change from hashes to class

--- store_item.rb
+++ store_item.rb
@@ -8,6 +8,48 @@ puts "Welcome to my store. "
 
 
 class Store
-def Initialize()
+  attr_reader :food_name, :food_color, :food_price
+  attr_writer: :food_name, :food_color, :food_price
 
+
+def initialize(input_food_name, input_food_color, input_food_price)
+  @food_name = input_food_name
+  @food_color = input_food_color
+  @food_price = input_food_price
 end  
+
+def food_name
+  @food_name
+end 
+
+def food_color
+  @food_color
+end 
+
+def food_price
+  @food_price
+end 
+
+def food_name=(input_text)
+  @food_name = input_text
+end 
+
+end 
+
+
+
+
+
+item1 = Store.new("Banana", "Yellow", 1.5)
+item2 = Store.new("Mango", "Orange", 2)
+item3 = Store.new("Kiwi", "Green", 3)
+
+puts item1.food_name
+puts item1.food_color
+puts item1.food_price
+puts item2.food_name
+puts item2.food_color
+puts item2.food_price
+puts item3.food_name
+puts item3.food_color
+puts item3.food_price
