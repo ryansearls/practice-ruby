121af6b2cbbbd00f8fc17c2525cb80775987c36e
Ryan Searls <Ryan.searls@gmail.com>
2021-06-10 12:08:42 -0500

    changed to hash options

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,6 @@
-item1 = {food: "banana", color: "yellow", price: 1.5}
-item2 = {food: "mango", color: "orange", price: 2.5}
-item3 = {"food" => "kiwi", "color" => "greeen", "price" => 3.0}
+# item1 = {food: "banana", color: "yellow", price: 1.5}
+# item2 = {food: "mango", color: "orange", price: 2.5}
+# item3 = {food: "kiwi", color: "greeen", price: 3.0}
 
 puts "Welcome to my store. "
 
@@ -9,13 +9,13 @@ puts "Welcome to my store. "
 
 class Store
   attr_reader :food_name, :food_color, :food_price
-  attr_writer: :food_name, :food_color, :food_price
+  attr_writer :food_name, :food_color, :food_price
 
 
-def initialize(input_food_name, input_food_color, input_food_price)
-  @food_name = input_food_name
-  @food_color = input_food_color
-  @food_price = input_food_price
+def initialize(input_options)
+  @food_name = input_options[:food_name]
+  @food_color = input_options[:food_color]
+  @food_price = input_options[:food_price]
 end  
 
 def food_name
@@ -40,9 +40,9 @@ end
 
 
 
-item1 = Store.new("Banana", "Yellow", 1.5)
-item2 = Store.new("Mango", "Orange", 2)
-item3 = Store.new("Kiwi", "Green", 3)
+item1 = Store.new(food_name: "Banana", food_color: "Yellow", food_price: 1.5)
+item2 = Store.new(food_name: "Mango", food_color: "Orange", food_price: 2)
+item3 = Store.new(food_name: "Kiwi", food_color: "Green", food_price: 3)
 
 puts item1.food_name
 puts item1.food_color
