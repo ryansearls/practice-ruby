7877e5b5fb5877dbed6d611706a0a1940928f4bd
Ryan Searls <Ryan.searls@gmail.com>
2021-10-14 13:05:45 -0600

    adds money solution

--- algo-ladder/basic_string.rb
+++ algo-ladder/basic_string.rb
@@ -3,19 +3,44 @@
 # Input: “abcde”
 # Output: “edcba”
 
-def reverse(string)
-  index = string.length - 1
-  new_string = ''
-  while index >= 0
-    new_string << string[index]
-    index -= 1
-  end 
+# def reverse(string)
+#   index = string.length - 1
+#   new_string = ''
+#   while index >= 0
+#     new_string << string[index]
+#     index -= 1
+#   end 
   
-  new_string
+#   new_string
 
+# end 
+
+# p reverse("abcde")
+
+
+
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
+
+
+
+def money(string)
+  index = 0
+  while index < string.length
+    if string[index] == "$"
+      return true
+    end   
+    index += 1 
+  end 
+  return false 
 end 
 
-p reverse("abcde")
+p money("abcdefghijklmnopqrstuvwxyz")
 
 
 
