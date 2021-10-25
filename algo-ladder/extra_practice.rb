b14b31848698ae6e12f008cb986fbac53d031e79
Ryan Searls <Ryan.searls@gmail.com>
2021-10-25 17:36:45 -0600

    reverse words

--- algo-ladder/extra_practice.rb
+++ algo-ladder/extra_practice.rb
@@ -334,3 +334,28 @@
 
 
 #================================================
+
+
+# Given a string of words, write a function that returns a new string that contains the words in reverse order.
+
+# Input: “popcorn is so cool isn’t it yeah i thought so”
+# Output: “so thought i yeah it isn’t cool so is popcorn”
+
+
+def reverse(string)
+  words = string.split(" ")
+  new_array = []
+  index = words.length - 1
+
+  while index >= 0
+    new_array << words[index]
+    index -= 1
+  end 
+  new_array.join(" ")  
+end   
+
+
+p reverse("popcorn is so cool isn’t it yeah i thought so")
+
+
+#=============================================================
