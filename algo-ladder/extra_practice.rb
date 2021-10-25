184448794317129d9a155f3ea6ee93128096a303
Ryan Searls <Ryan.searls@gmail.com>
2021-10-25 17:24:47 -0600

    hamming

--- algo-ladder/extra_practice.rb
+++ algo-ladder/extra_practice.rb
@@ -272,23 +272,37 @@
 # Output: false
 
 
-def palindrome?(string)
-  index = 0
-  while index < (string.length / 2)
-    if string[index] != string[-(index + 1)]
-      return false
-    else 
-      index += 1
-    end 
-  end   
-  return true    
-end 
+# def palindrome?(string)
+#   index = 0
+#   while index < (string.length / 2)
+#     if string[index] != string[-(index + 1)]
+#       return false
+#     else 
+#       index += 1
+#     end 
+#   end   
+#   return true    
+# end 
 
-p palindrome?("racecar")
-
-
+# p palindrome?("racecar")
 
 
 #====================================
 
 
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+
+def hamming(string1, string2)
+  3
+
+
+#================================================
