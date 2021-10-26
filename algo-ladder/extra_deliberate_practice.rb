e1c64269c0b77ef3f2e62333f24790fba7bfebc4
Ryan Searls <Ryan.searls@gmail.com>
2021-10-26 15:42:41 -0600

    string length

--- algo-ladder/extra_deliberate_practice.rb
+++ algo-ladder/extra_deliberate_practice.rb
@@ -5,16 +5,41 @@
 # Output: [8, 9, 10]
 
 
-def plus_seven(array)
-index = 0
-new_array = []
-  while index < array.length
-    new_array << array[index] + 7
-    index += 1
-  end 
-  new_array   
+# def plus_seven(array)
+# index = 0
+# new_array = []
+#   while index < array.length
+#     new_array << array[index] + 7
+#     index += 1
+#   end 
+#   new_array   
 
+# end 
+
+# p plus_seven([1,2,3])
+
+
+
+#=======================================
+
+#2
+# Write a function that accepts an array of strings and returns an array with each strings length.
+
+
+# Example:
+# Input: ["hello", "goodbye"]
+# Output: [5, 7]
+
+
+def string_length(array)
+  index = 0
+  new_array = []
+  while index < array.length
+    new_array << array[index].length
+    index += 1
+  end   
+  new_array 
 end 
 
-p plus_seven([1,2,3])
+p string_length(["hello", "goodbye"])
 
