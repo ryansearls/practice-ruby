b0b400e70bd7dd473b30a56a814931169896be32
Ryan Searls <Ryan.searls@gmail.com>
2021-10-27 12:51:42 -0600

    prime numbers

--- algo-ladder/basic_numbers.rb
+++ algo-ladder/basic_numbers.rb
@@ -1,19 +1,23 @@
 # Write a function that returns whether a given number is a prime number.
 
 def prime?(number)
-  # i = 2
-  # while index < number
-  if number % i == 0 && number > 1 && number != i
+  if number < 2
     return false
-  else 
-    return true
-  end 
-  # index += 1
-end 
+  end   
 
-p prime?(13) #true
-p prime?(7) #true
-p prime?(2) #true
-p prime?(8) #false
-p prime?(11) #true
-p prime?(15) #false
+  i = 2
+  while i < number
+    return false if number % i == 0
+    i += 1
+  end
+  true
+end
+
+p prime?(7) 
+
+# p prime?(13) #true
+# p prime?(7) #true
+# p prime?(2) #true
+# p prime?(8) #false
+# p prime?(11) #true
+# p prime?(15) #false
