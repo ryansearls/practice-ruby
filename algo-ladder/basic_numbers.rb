6caea14bc4439a4bc86904372a38537cb6429b1d
Ryan Searls <Ryan.searls@gmail.com>
2021-10-27 13:22:21 -0600

    fizzbuzz

--- algo-ladder/basic_numbers.rb
+++ algo-ladder/basic_numbers.rb
@@ -1,19 +1,19 @@
 # Write a function that returns whether a given number is a prime number.
 
-def prime?(number)
-  if number < 2
-    return false
-  end   
+# def prime?(number)
+#   if number < 2
+#     return false
+#   end   
 
-  i = 2
-  while i < number
-    return false if number % i == 0
-    i += 1
-  end
-  true
-end
+#   i = 2
+#   while i < number
+#     return false if number % i == 0
+#     i += 1
+#   end
+#   true
+# end
 
-p prime?(7) 
+
 
 # p prime?(13) #true
 # p prime?(7) #true
@@ -21,3 +21,44 @@ p prime?(7)
 # p prime?(8) #false
 # p prime?(11) #true
 # p prime?(15) #false
+
+
+#============================================
+
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+def fizzbuzz(number)
+  index = 1
+  while index <= number
+    if index % 3 == 0 && index % 5 == 0
+      puts "FIZZBUZZ"
+    elsif index % 3 == 0 
+      puts "FIZZ"
+    elsif index % 5 == 0 
+      puts "BUZZ"
+    else !(index % 3 == 0 || index % 5 == 0)
+      puts "Neither"
+    end       
+    index += 1
+  end   
+end 
+
+fizzbuzz(17)
+
+
+
+
+
+
+
+
+
+
+
+
+#===================================
