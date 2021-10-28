e46c4efcef9d03cfbee50f558ad670a71e41276b
Ryan Searls <Ryan.searls@gmail.com>
2021-10-28 17:10:49 -0600

    array mesh II

--- algo-ladder/two_pointers.rb
+++ algo-ladder/two_pointers.rb
@@ -1,6 +1,45 @@
-Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
+# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
 
-Input: ["a", "b", "c"], ["d", "e", "f", "g"]
-Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+# Input: ["a", "b", "c"], ["d", "e", "f", "g"]
+# Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
+
+
+# def string_combo(array1, array2)
+#   index = 0
+#   array = []
+#   while index 
+
+
+# end 
+
+
+# p string_combo(["a", "b", "c"], ["d", "e", "f", "g"])
+
+
+#==================================================================
+
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+def meshii(array)
+  new_array = []
+  i1 = 0
+  while i1 < array.length
+    i2 = 0
+    while i2 < array.length
+      if array[i1] != array[i2]
+        new_array << array[i1] + array[i2]
+      end 
+      i2 += 1
+    end 
+    i1 += 1
+  end 
+  new_array         
+end 
+
+
+p meshii(["a", "b", "c", "d"])
 
 
