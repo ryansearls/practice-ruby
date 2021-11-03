89c7a0c1fecb1113d251dd9b14eb4b2eccd93201
Ryan Searls <Ryan.searls@gmail.com>
2021-11-03 14:02:09 -0600

    basic hash update

--- algo-ladder/basic_hash.rb
+++ algo-ladder/basic_hash.rb
@@ -8,17 +8,22 @@ def frequent_letter(string)
   count = {}
   index = 0
   most_frequent_letter = ''
+  most_frequent_count = 0
   while index < string.length
     if count[string[index]] 
       count[string[index]] += 1
     else 
-      count[string[index]] += 1
+      count[string[index]] = 1
     end 
+
+    if count[string[index]] > most_frequent_count
+      most_frequent_count = count[string[index]]
+      most_frequent_letter = string[index]
+    end 
+
     index += 1
   end 
-  return most_frequent_letter 
-
-
+  most_frequent_letter 
 end 
 
 
@@ -40,27 +45,25 @@ p frequent_letter("peter piper picked a peck of pickled peppers")
 
 
 
-def votes(candidates)
-  index = 0
-  total_votes = {"Dewey" => 0, "Truman" => 0}
+# def votes(candidates)
+#   index = 0
+#   total_votes = {"Dewey" => 0, "Truman" => 0}
 
-  while index < candidates.length
-    if candidates[index] == "Dewey"
-      total_votes["Dewey"] += 1
-    else candidates[index] == "Truman"
-      total_votes["Truman"] += 1
-    end 
-    index += 1
-  end   
-  total_votes
+#   while index < candidates.length
+#     if candidates[index] == "Dewey"
+#       total_votes["Dewey"] += 1
+#     else candidates[index] == "Truman"
+#       total_votes["Truman"] += 1
+#     end 
+#     index += 1
+#   end   
+#   total_votes
 
-end   
+# end   
 
-p votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
+# p votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
 
-#============================
-
-
+#================================================================================
 
 
 
@@ -81,33 +84,35 @@ p votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Trum
 
 
 
-Given a DNA strand, return its RNA complement (per RNA transcription).
-
-Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
-
-Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
-
-G becomes C
-C becomes G
-T becomes A
-A becomes U
-
-So based on all this, here's a sample input/output:
-
-Input: 'ACGTGGTCTTAA'
-Output: 'UGCACCAGAAUU'
 
 
-def rna(string)
-  complementary_letters = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
-  index = 0
-  rna = ""
-  while index < string.length
-    rna << complementary_letters[string[index]]
-    index += 1
-  end
-  return rna
-end
+# Given a DNA strand, return its RNA complement (per RNA transcription).
 
-p rna("ACGTGGTCTTAA")
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, here's a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+
+
+# def rna(string)
+#   complementary_letters = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
+#   index = 0
+#   rna = ""
+#   while index < string.length
+#     rna << complementary_letters[string[index]]
+#     index += 1
+#   end
+#   return rna
+# end
+
+# p rna("ACGTGGTCTTAA")
 
