7738ef91945f2abeb97e8b4d84325a0255a6a426
Ryan Searls <Ryan.searls@gmail.com>
2021-11-04 19:41:14 -0600

    popular posts

--- algo-ladder/basic_hash.rb
+++ algo-ladder/basic_hash.rb
@@ -76,37 +76,42 @@
 
 
 
-def entire_menu(hash)
-  total = 0
-  index = 0
-  while index < hash.length
-    total += hash[index].to_i
-    index += 1
-  end 
-  total
+# def entire_menu(hash)
+#   total = 0
+#   index = 0
+#   while index < hash.length
+#     total += hash[index].to_i
+#     index += 1
+#   end 
+#   total
 
-end 
-
-
-p entire_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+# end 
 
 
+# p entire_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+
+
+#=====================================================================
+
+Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+Input: [
+{title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+{title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+{title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+{title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+]
+
+Output: [
+{title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+{title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+]
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+#==========================================================
 
 # Given a DNA strand, return its RNA complement (per RNA transcription).
 
@@ -137,4 +142,7 @@ p entire_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" 
 # end
 
 # p rna("ACGTGGTCTTAA")
+
+
+#==================================================
 
