75efd64e5046975f95b681d0d61f6bc49db0610b
Ryan Searls <Ryan.searls@gmail.com>
2021-11-16 18:24:59 -0700

    merge sorted arrays

--- algo-ladder/two_pointers.rb
+++ algo-ladder/two_pointers.rb
@@ -77,3 +77,16 @@ Output: false (While 1, 2, 3, and 4 altogether add up to 10, were seeking just o
 
 
 #==============================================
+
+
+Given two sorted arrays, merge the second array into the first array while ensuring that the first array remains sorted. Do not use any built-in sort methods.
+
+Input :
+A : [1, 5, 8]
+B : [6, 9]
+
+Modified A : [1, 5, 6, 8, 9]
+
+
+
+#================================================
