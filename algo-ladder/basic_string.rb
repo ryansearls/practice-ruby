7566918e966aee33cdabc07fc4aa6334d828324d
Ryan Searls <Ryan.searls@gmail.com>
2021-11-17 15:59:25 -0700

    alternate capitals

--- algo-ladder/basic_string.rb
+++ algo-ladder/basic_string.rb
@@ -31,29 +31,23 @@
 
 
 
-def show_me_the_money(string)
-  index = 0
-  while index < string.length
-    if string[index] == "$"
-      return true
-    end   
-    index += 1 
-  end 
-  return false 
-end 
+# def show_me_the_money(string)
+#   index = 0
+#   while index < string.length
+#     if string[index] == "$"
+#       return true
+#     end   
+#     index += 1 
+#   end 
+#   return false 
+# end 
 
-p show_me_the_money("abcdefgh$ijklmnopqrstuvwxyz")
-p show_me_the_money("abcdefghijklmnopqrstuvwxyz")
-p show_me_the_money("i hate $ but i love money i know i know im crazy")
+# p show_me_the_money("abcdefgh$ijklmnopqrstuvwxyz")
+# p show_me_the_money("abcdefghijklmnopqrstuvwxyz")
+# p show_me_the_money("i hate $ but i love money i know i know im crazy")
 
 
 #======================================
-
-
-
-
-
-
 
 
 # Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
@@ -77,37 +71,24 @@ p show_me_the_money("i hate $ but i love money i know i know im crazy")
 =end
 
 
-# def upcase(string)
-#   index = 0
-#   index2 = 1
-#   new_string = ''
-#   while index < string.length
-#     new_string << string[index].upcase 
-#     index += 2
-#   end 
-#   return new_string
-# end 
+def upcase(string)
+  index1 = 0
+  index2 = 1
+  new_string = ''
+  while index2 < string.length
+    new_string << string[index1]
+    index1 += 2
+    new_string << string[index2].to_s.upcase
+    index2 += 2
+  end 
+  new_string
+
+end 
+
+p upcase("hello, how are your porcupines today?")
 
 
-# p upcase("hello, how are your porcupines today?")
-
-
-
-# def upcase(string)
-#   index = 0
-#   index2 = 1
-#   new_string = ''
-#   while index < string.length
-#     new_string << string[index]
-#     index += 2
-#     new_string << string[index2].to_s.upcase
-#     index2 += 2
-#   end 
-#   return new_string
-# end 
-
-
-# p upcase("hello, how are your porcupines today?")
+#==========================================
 
 
 # def duplicate(string)
@@ -120,7 +101,7 @@ p show_me_the_money("i hate $ but i love money i know i know im crazy")
 #     end 
 # end 
 
-# p duplicate("abcdefghhijkkloooop")
+# p duplicate()
 
 
 
