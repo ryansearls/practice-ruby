9b3ceb4b1fd04b62e0cc33ff0fdf38ed222b3de9
Ryan Searls <Ryan.searls@gmail.com>
2021-11-17 14:28:21 -0700

    reverse array

--- algo-ladder/basic_array.rb
+++ algo-ladder/basic_array.rb
@@ -143,19 +143,22 @@ end while loop
 # Output: [5, 4, 3, 2, 1]
 
 
-# def reverse_array(array)
-#   new_arr = []
-#   i = array.length - 1
 
-#   while i >= 0 
-#     new_arr << array[i] 
-#     i -= 1
-#   end
+def reverse(array)
+  index = array.length - 1
+  new_array = []
+  while index >= 0
+    new_array << array[index]
+    index -= 1
+  end 
+  new_array  
+end 
 
-#   new_arr 
-# end
 
-# p reverse_array([1, 2, 3, 4, 5])   #=> [5, 4, 3, 2, 1]
+p reverse([1, 2, 3, 4, 5])
+
+
+#===================================
 
 
 # Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
