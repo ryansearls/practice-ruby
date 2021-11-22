6f7bd730b46493e27e89ef6607331e4aadbecaf6
Ryan Searls <Ryan.searls@gmail.com>
2021-11-22 15:46:36 -0700

    is valid email?

--- james_practice_problems/ruby_problems.rb
+++ james_practice_problems/ruby_problems.rb
@@ -32,32 +32,81 @@
 
 
 
-def is_valid_name?(string)
-  parts = string.split(" ")
+# def is_valid_name?(string)
+#   parts = string.split(" ")
 
-  if parts.length < 2
+#   if parts.length < 2
+#     return false
+#   end 
+  
+#   index = 0
+#   while index < parts.length
+#     if parts[index][0] == parts[index][0].upcase && parts[index][1..-1] == parts[index][1..-1].downcase
+#       return true
+#     else 
+#       return false
+#     end 
+#     index += 1
+#   end     
+
+
+# end
+
+# p is_valid_name?("Kush Patel")       # => true
+# p is_valid_name?("Daniel")           # => false
+# p is_valid_name?("Robert Downey Jr") # => true
+# p is_valid_name?("ROBERT DOWNEY JR") # => false
+
+# ["Kush", "Patel"]
+
+
+#=======================================================
+
+# Is Valid Email
+# Write a method is_valid_email that takes in a string and returns a boolean indicating whether or not it is a valid email address.
+# For simplicity, we'll consider an email valid when it satisfies all of the following:
+# - contains exactly one @ symbol
+# - contains only lowercase alphabetic letters before the @
+# - contains exactly one . after the @
+
+
+
+
+def is_valid_email?(string)
+  parts = string.split("@")
+  
+  if parts.length != 2
     return false
   end 
   
+  first_part = parts[0]
+  second_part = parts[1]
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+
   index = 0
-  while index < parts.length
-    if parts[index][0] == parts[index][0].upcase && parts[index][1..-1] == parts[index][1..-1].downcase
-      return true
-    else 
+  while index < first_part.length
+    char = first_part[index]
+    if !alpha.include?(char)
       return false
     end 
     index += 1
+  end 
+  
+  if second_part.split(".").length == 2
+    return true
+  else 
+    return false
   end     
 
 
 end
 
-p is_valid_name?("Kush Patel")       # => true
-p is_valid_name?("Daniel")           # => false
-p is_valid_name?("Robert Downey Jr") # => true
-p is_valid_name?("ROBERT DOWNEY JR") # => false
+p is_valid_email?("abc@xy.z")         # => true
+p is_valid_email?("jdoe@gmail.com")   # => true
+p is_valid_email?("jdoe@g@mail.com")  # => false
+p is_valid_email?("jdoe42@gmail.com") # => false
+p is_valid_email?("jdoegmail.com")    # => false
+p is_valid_email?("az@email")         # => false
 
-["Kush", "Patel"]
 
-
-#=======================================================
+#===========================================
