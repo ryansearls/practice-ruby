c120c0ab4b53628ecde19829e25c64bc150aeb39
Ryan Searls <Ryan.searls@gmail.com>
2021-12-01 14:03:48 -0700

    conditionals 8/9/10

--- skills-sheet/conditionals_2.rb
+++ skills-sheet/conditionals_2.rb
@@ -91,3 +91,45 @@
 # end
 
 #==========================================
+
+# Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# number = 3
+# if number == 100
+#   p 100
+# elsif number == 99
+#   p 99
+# else
+#   p 0
+# end
+
+#===========================================
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# number1 = -4
+# number2 = -9
+# if number1 < 0 && number2 > 0
+#   p 1
+# else
+#   p 0
+# end
+
+#===============================================
+
+# Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+grade = 72
+if grade > 80
+  p 5
+elsif grade > 60
+  p 4
+elsif grade > 40
+  p 3
+elsif grade > 20
+  p 2
+else
+  p 1
+end
+
+#===================================================
