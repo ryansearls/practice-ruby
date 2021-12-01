9b2fd162598a687099ecc40934e4db09e0577e95
Ryan Searls <Ryan.searls@gmail.com>
2021-12-01 13:33:52 -0700

    conditionals #1

---
+++ skills-sheet/conditionals_2.rb
@@ -0,0 +1,13 @@
+# Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+num = 1
+if num == 10
+  p 0
+else 
+  p -1  
+end 
+
+#=========================================================
+
+
+Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
