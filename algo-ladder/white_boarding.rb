cf4483cf6f3009325d9ef972f90983002d8e0d6a
Ryan Searls <Ryan.searls@gmail.com>
2021-12-01 15:04:52 -0700

    basic bash built in methods

--- algo-ladder/white_boarding.rb
+++ algo-ladder/white_boarding.rb
@@ -136,27 +136,47 @@ return total_votes
 # ]
 
 
-def popular_posts(array)
-  new_array = []
+# def popular_posts(array)
+#   new_array = []
 
-  array.each do |hash|
-    hash.each do |k, v|
-     if hash[:likes] >= 1000
-      new_array << hash 
-      break 
-     end 
-    end 
-  end 
-  new_array   
-end 
+#   array.each do |hash|
+#     hash.each do |k, v|
+#      if hash[:likes] >= 1000
+#       new_array << hash 
+#       break 
+#      end 
+#     end 
+#   end 
+#   new_array   
+# end 
 
 
-p popular_posts([
-  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
-  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
-  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
-  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
-  ])
+# p popular_posts([
+#   {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+#   {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+#   {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+#   {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+#   ])
 
 
 #===========================================================
+
+
+Given a DNA strand, return its RNA complement (per RNA transcription).
+
+Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+G becomes C
+C becomes G
+T becomes A
+A becomes U
+
+So based on all this, here's a sample input/output:
+
+Input: 'ACGTGGTCTTAA'
+Output: 'UGCACCAGAAUU'
+
+
+hash = {}
