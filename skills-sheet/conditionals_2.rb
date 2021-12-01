80d5232a69f2f362035f89dffc8596c58fb9704f
Ryan Searls <Ryan.searls@gmail.com>
2021-12-01 13:52:13 -0700

    conditionals 7

--- skills-sheet/conditionals_2.rb
+++ skills-sheet/conditionals_2.rb
@@ -80,3 +80,14 @@
 # end
 
 #==========================================
+
+# Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# number = -345
+# if number < 0
+#   p 1776
+# else
+#   p 1979
+# end
+
+#==========================================
