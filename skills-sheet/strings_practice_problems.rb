e234d31993b648a22560de9e99f7c4623b80ebff
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 19:18:59 -0700

    strings 5/6

--- skills-sheet/strings_practice_problems.rb
+++ skills-sheet/strings_practice_problems.rb
@@ -27,8 +27,31 @@
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-color1 = "red"
-color2 = "blue"
-color3 = "green"
+# color1 = "red"
+# color2 = "blue"
+# color3 = "green"
 
-puts "my favorite colors are " + color1 + " and " + color2 + " and " + color3 + "."
+# puts "my favorite colors are " + color1 + " and " + color2 + " and " + color3 + "."
+
+#============================================================================
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# color1 = "red"
+# color2 = "green"
+# color3 = "blue"
+# puts "My favorite colors are #{color1}, #{color2}, and #{color3}."
+
+#===============================================================================
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+puts "What is your name?"
+name = gets.chomp
+if name != "Santa"
+  puts "You're not Santa."
+end
+
+#================================================================================
+
+
