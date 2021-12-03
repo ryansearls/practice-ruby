8073e1f70b5e2bb1484672a9ce8515de4f5be317
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 19:35:21 -0700

    loops 1/2

---
+++ skills-sheet/loops2.rb
@@ -0,0 +1,24 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# number = 1
+# while number < 11
+#   p number
+#   number += 1
+# end   
+
+#=============================================================
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+
+5. times do 
+  p "hello"
+end   
+
+count = 0
+while count < 5
+  puts "hello"
+  count = count + 1
+end
+
+#===============================================================
+
