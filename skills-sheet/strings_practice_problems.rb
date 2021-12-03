553aa82f9ec7150a02b29fb3e8d19f29e06cae12
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 19:09:33 -0700

    strings 1/2

--- skills-sheet/strings_practice_problems.rb
+++ skills-sheet/strings_practice_problems.rb
@@ -1,2 +1,17 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+# first_name = "ryan"
+# last_name = "searls"
+
+# p first_name + " " + last_name
+
+#=====================================================================
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# first_name = "ryan"
+# last_name = "searls"
+
+# puts "my first name is #{first_name} and my last name is #{last_name}"
+
+#=======================================================================
