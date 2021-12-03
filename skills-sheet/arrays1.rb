fd38291297118f2167306fd8d5f1c2093935d265
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 21:15:49 -0700

    arrays1 solutions

---
+++ skills-sheet/arrays1.rb
@@ -0,0 +1,98 @@
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+# array = ["yo", "hello", "bye"]
+# array << "but"
+# array << "yel"
+
+# p array 
+
+#====================================================================
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# letters = ["a", "r", "w", "p"]
+# letters[1] = 32
+# p letters 
+
+#======================================================================
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+# numbers = [3, 2, 1, 5, 3]
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end 
+
+#=========================================================================
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+# array = [2]
+# array << 4
+# array << 5
+# array << 6
+
+# p array 
+
+#===========================================================================
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# array = ["hello", "yo", "bye"]
+# array[2] = array[2].upcase
+
+# p array 
+
+#=========================================================================
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+# array = ["ryan", "kyle", "bill"]
+# index = 0
+# while index < array.length
+#   p array[index]
+#   index += 1
+# end   
+
+#==========================================================================
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# strings = ["black", "white"]
+# strings << "gray"
+
+# p strings
+
+#============================================================================
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# numbers = [3, 4, 24, 3, 1]
+# numbers[0] = numbers[0] * 10
+
+# p numbers
+
+#==========================================================================
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+# numbers = [93, 23]
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index = index + 1
+# end
+
+#========================================================================
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# countries = ["France", "Korea", "Brazil"]
+# countries << "Mexico"
+
+# p countries
+
+#====================================================================
+
+
