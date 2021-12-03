5bf795dd6f5ade769a7c8e417afebecf1e777bf3
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 19:24:57 -0700

    strings 9/10

--- skills-sheet/strings_practice_problems.rb
+++ skills-sheet/strings_practice_problems.rb
@@ -70,3 +70,24 @@
 
 #===============================================================================
 
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# puts "Enter the password: "
+# password = gets.chomp
+# if password == "Joshua"
+#   puts "Shall we play a game?"
+# else
+#   puts "Access denied"
+# end
+
+#===================================================================================
+
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+# city1 = "Chicago"
+# city2 = "New York"
+# city3 = "San Francisco"
+# puts city1 + ", " + city2 + ", and " + city3 + " are three major cities in the United States."
+
+#================================================================================
