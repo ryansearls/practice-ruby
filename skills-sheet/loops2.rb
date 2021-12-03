f1f664e8bc965606d327427f0a9bb52d0b2dc6d2
Ryan Searls <Ryan.searls@gmail.com>
2021-12-02 19:58:00 -0700

    loops 7/8/9/10

--- skills-sheet/loops2.rb
+++ skills-sheet/loops2.rb
@@ -56,12 +56,57 @@
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
-while true
-  puts "Enter a number: "
-  input = gets.chomp
-  if input.to_i > 10
-    break
-  end
-end
+# while true
+#   puts "Enter a number: "
+#   input = gets.chomp
+#   if input.to_i > 10
+#     break
+#   end
+# end
 
 #========================================================================
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# number = 50
+# while number <= 70
+#   p number
+#   number = number + 1
+# end
+
+#========================================================================
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# count = 0
+# while count < 144
+#   puts "Around the world"
+#   count = count + 1
+# end
+
+#=========================================================================
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   puts "Enter a word: "
+#   input = gets.chomp
+#   if input.length > 5
+#     break
+#   end
+# end
+
+#=========================================================================
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+
+number = 2
+while number <= 40
+  p number
+  number += 2
+end
+
+#=======================================================================
+
+
+
