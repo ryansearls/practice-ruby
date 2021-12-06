8d63aba5cba6a447993314be41258cfd990534ff
Ryan Searls <Ryan.searls@gmail.com>
2021-12-06 14:38:07 -0700

    function subtraction

--- skills-sheet/functions_practice.rb
+++ skills-sheet/functions_practice.rb
@@ -1,9 +1,31 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
-def double(number)
-  number = number * 2
-end 
+# def double(number)
+#   number = number * 2
+# end 
 
-p double(4)
+# p double(4)
 
 #========================================================
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def all_caps(string)
+#   string = string.upcase
+
+# end 
+
+# p all_caps("hello")
+
+#==========================================================
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+# def subtract(num1, num2)
+#   return num1 - num2
+
+# end 
+
+# p subtract(8, 5)
+
+#==============================================================
