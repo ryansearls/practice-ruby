16fa180e887e84b988f583277186e0e683abaf54
Ryan Searls <Ryan.searls@gmail.com>
2021-12-06 14:33:18 -0700

    fuctions practice 1

--- skills-sheet/functions_practice.rb
+++ skills-sheet/functions_practice.rb
@@ -1,2 +1,9 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+def double(number)
+  number = number * 2
+end 
+
+p double(4)
+
+#========================================================
