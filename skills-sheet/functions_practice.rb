b9c7eb2ae6a9112cd7c4bafc6d9f30efb7832ea9
Ryan Searls <Ryan.searls@gmail.com>
2021-12-06 15:30:35 -0700

    functions 6/7

--- skills-sheet/functions_practice.rb
+++ skills-sheet/functions_practice.rb
@@ -43,10 +43,32 @@
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
-def get_first_letter(string)
-  return string[0]
-end
+# def get_first_letter(string)
+#   return string[0]
+# end
 
-p get_first_letter("something")
+# p get_first_letter("something")
 
 #=============================================================
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+# def string_combiner(string1, string2, string3)
+#   return "#{string1} #{string2} #{string3}"
+# end
+
+# p string_combiner("the", "cat", "jumped")
+
+#===============================================================
+
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def convert_to_string(number)
+  return number.to_s
+end
+
+p convert_to_string(4)
+
+#=============================================================
+
