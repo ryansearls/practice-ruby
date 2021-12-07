c119a0ad762c6f052ad5608e3bfa31772cead918
Ryan Searls <Ryan.searls@gmail.com>
2021-12-07 13:00:30 -0700

    array upcase map

--- skills-sheet/array_map.rb
+++ skills-sheet/array_map.rb
@@ -1,24 +1,31 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 
-def double(array)
-  new_array = []
-  array.each do |num|
-    new_array << num * 3
-  end   
-  new_array
+# def double(array)
+#   new_array = []
+#   array.each do |num|
+#     new_array << num * 3
+#   end   
+#   new_array
 
-end 
+# end 
 
-p double([1,3,4])
+# p double([1,3,4])
 
 #====================================================================
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 
-def upcase(string)
+# def upcase(array)
+#   new_array = []
+#   index = 0
+#   while index < array.length
+#     new_array << array[index].upcase
+#     index += 1
+#   end 
+#   new_array  
   
+# end 
 
-end 
+# p upcase(["hello", "yooo"])
 
-
-p upcase("hello")
+#=====================================================================
