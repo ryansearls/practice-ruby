530b4762a5d2bb08e4615a194914a434d3bfbe32
Ryan Searls <Ryan.searls@gmail.com>
2021-12-07 14:37:47 -0700

    array map 5

--- skills-sheet/array_map.rb
+++ skills-sheet/array_map.rb
@@ -53,14 +53,35 @@
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
-def plus_seven(array)
-  new_array = []
-  array.each do |number|
-    new_array << number + 7
-  end 
-  new_array   
+# def plus_seven(array)
+#   new_array = []
+#   array.each do |number|
+#     new_array << number + 7
+#   end 
+#   new_array   
 
-end 
+# end 
 
-p plus_seven([1,2,3])
+# p plus_seven([1,2,3])
 #============================================================
+
+#  5. Start with an array of strings and create a new array with each string's length.
+#     For example, ["hello", "goodbye"] becomes [5, 7].
+
+# def string_length(array)
+#   new_array = []
+#   index = 0
+#   while index < array.length
+#     new_array << array[index].length
+#     index += 1
+#   end 
+  
+#   new_array
+
+# end 
+
+
+# p string_length(["hello", "goodbye"])
+
+#=============================================================
+
