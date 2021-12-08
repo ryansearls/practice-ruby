c622c82fdc99b288338a8f817a5f824c69944e12
Ryan Searls <Ryan.searls@gmail.com>
2021-12-07 18:38:54 -0700

    less than 10

--- skills-sheet/array_select.rb
+++ skills-sheet/array_select.rb
@@ -133,18 +133,45 @@
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
-def dont_start_with_b(array)
-  new_array = []
-  array.each do |string|
-    if string[0] != "b"
-      new_array << string
-    end 
-  end 
-  new_array
+# def dont_start_with_b(array)
+#   new_array = []
+#   array.each do |string|
+#     if string[0] != "b"
+#       new_array << string
+#     end 
+#   end 
+#   new_array
 
-end 
+# end 
 
 
-p dont_start_with_b(["big", "little", "good", "bad"])
+# p dont_start_with_b(["big", "little", "good", "bad"])
+
+#====================================================================
+
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+# def less_than_10(array)
+#   new_array = []
+#   array.each do |hash|
+#     if hash[:price] < 10
+#       new_array << hash 
+#     end 
+#   end 
+#   new_array
+
+
+# end 
+
+
+# p less_than_10([{name: "chair", price: 100}, 
+#               {name: "pencil", price: 1}, 
+#               {name: "book", price: 4}])
+
+#=================================================================
+
+# 10. Start with an array of numbers and create a new array with only the odd numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
 
 
