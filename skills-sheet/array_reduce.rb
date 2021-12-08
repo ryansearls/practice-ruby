18bc12d041f018db5745ce2c523198aa86b79293
Ryan Searls <Ryan.searls@gmail.com>
2021-12-07 19:36:56 -0700

    array sum

---
+++ skills-sheet/array_reduce.rb
@@ -0,0 +1,21 @@
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+
+def sum(array)
+  sum = 0
+  array.each do |number|
+    sum += number
+  end 
+  
+  sum 
+end 
+
+
+p sum([5, 10, 8, 3])
+
+#=================================================================
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+
+
