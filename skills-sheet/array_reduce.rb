9ddfd6f2c7f2ed319d116cb7147f01cccd5edf67
Ryan Searls <Ryan.searls@gmail.com>
2021-12-07 19:43:01 -0700

    array into single string

--- skills-sheet/array_reduce.rb
+++ skills-sheet/array_reduce.rb
@@ -1,21 +1,37 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 
-def sum(array)
-  sum = 0
-  array.each do |number|
-    sum += number
-  end 
+# def sum(array)
+#   sum = 0
+#   array.each do |number|
+#     sum += number
+#   end 
   
-  sum 
-end 
+#   sum 
+# end 
 
 
-p sum([5, 10, 8, 3])
+# p sum([5, 10, 8, 3])
 
 #=================================================================
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+# def single_string(array)
+#   string = ""
+#   array.each do |stringz|
+#     string << stringz
+#   end 
+  
+#   string 
+
+
+# end 
+
+
+# p single_string(["volleyball", "basketball", "badminton"])
+
+#====================================================================
 
 
 
