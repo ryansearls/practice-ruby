a5938fa045b4fe4d0bd8fdcbf7db503ef197e399
Ryan Searls <Ryan.searls@gmail.com>
2021-12-15 16:42:51 -0700

    over 1000 likes

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -207,18 +207,56 @@
 # Explanation: If someone would order one of everything on the menu, theyd pay a total of 14 (the sum of all the hashs values).
 
 
-def whole_menu(hash)
-  total = 0
-  hash.each do |k, v|
-    total += v
+# def whole_menu(hash)
+#   total = 0
+#   hash.each do |k, v|
+#     total += v
+#   end 
+#   total   
+
+# end 
+
+
+# p whole_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+
+#================================================================
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# ]
+
+def over_1000(array)
+  new_array = []
+  index = 0
+  while index < array.length
+    if array[index][:likes] >= 1000
+      new_array << array[index]
+    end 
+    index += 1
   end 
-  total   
+  new_array    
+
+
 
 end 
 
 
-p whole_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
-
+p over_1000([
+  {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+  {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+  {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+  {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+  ])
 
 
 
