a034d4df997a5ef2b7fbb3f46f11dd581a953a2b
Ryan Searls <Ryan.searls@gmail.com>
2021-12-15 16:05:35 -0700

    highest value

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -48,17 +48,40 @@
 # Input: [4, 2, 5, 99, -4]
 # Output: [8, 4, 10, 198, -8]
 
-def double(array)
-  new_array = []
+# def double(array)
+#   new_array = []
   
-  array.each do |number|
-    new_array << number * 2
-  end 
-  new_array  
-end 
+#   array.each do |number|
+#     new_array << number * 2
+#   end 
+#   new_array  
+# end 
 
 
-p double([4, 2, 5, 99, -4])
+# p double([4, 2, 5, 99, -4])
 
 #=================================================================
+
+# Write a function that returns the greatest value from an array of numbers.
+
+# Input: [5, 17, -4, 20, 12]
+# Output: 20
+
+def max(array)
+  high = array[0]
+  index = 1
+  while index < array.length
+    if array[index] > high
+      high = array[index]
+    end 
+    index += 1
+  end 
+  high    
+
+
+end 
+
+p max([5, 17, -4, 20, 12])
+
+#==========================================
 
