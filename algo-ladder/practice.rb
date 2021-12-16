bb8de1bda85563618d1ce813382274f5ed0b0370
Ryan Searls <Ryan.searls@gmail.com>
2021-12-15 17:54:04 -0700

    palindrome

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -435,6 +435,36 @@
 
 #====================================================
 
+# Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+# Input: “racecar”
+# Output: true
+
+# Input: “baloney”
+# Output: false
+
+def palindrome?(string)
+  index1 = 0
+  index2 = string.length - 1
+  while index1 < string.length
+    if string[index1] != string[index2]
+      return false
+    end 
+    index1 += 1
+    index2 -= 1
+  end 
+  return true   
+
+
+end 
+
+
+p palindrome?("racecar")
+p palindrome?("rcar")
+p palindrome?("dfbdjsbnvsn")
+
+#==============================================================
+
 
 
 
