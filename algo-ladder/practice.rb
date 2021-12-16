88d383b310245f7bda9328b844bc67b0896ed88c
Ryan Searls <Ryan.searls@gmail.com>
2021-12-16 13:59:12 -0700

    fib

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -564,23 +564,43 @@
 # p fizzbuzz(16)
 
 
-def fizzbuzz(number)
-  index = 1
-  while index <= number
-    if index % 3 == 0 && index % 5 == 0
-      puts "FIZZBUZZ"
-    elsif index % 3 == 0 
-      puts "FIZZ"
-    elsif index % 5 == 0 
-      puts "BUZZ"
-    elsif !(index % 3 == 0 || index % 5 == 0)
-      puts index 
-    end       
-    index += 1
-  end   
-end 
+# def fizzbuzz(number)
+#   index = 1
+#   while index <= number
+#     if index % 3 == 0 && index % 5 == 0
+#       puts "FIZZBUZZ"
+#     elsif index % 3 == 0 
+#       puts "FIZZ"
+#     elsif index % 5 == 0 
+#       puts "BUZZ"
+#     elsif !(index % 3 == 0 || index % 5 == 0)
+#       puts index 
+#     end       
+#     index += 1
+#   end   
+# end 
 
-fizzbuzz(17)
+# fizzbuzz(17)
+
+#=======================================================
+
+# 3
+
+def fib(number)
+  num1, num2 = [0,1]
+
+  (number - 1).times do
+    num1, num2 = num2, num1 + num2
+  end 
+  
+  num1
+end   
+
+p fib(9)
+
+#=====================================================
+  
+
 
 
 
