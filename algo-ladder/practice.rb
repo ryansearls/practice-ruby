0a0aa8e14a8bfb07b9df6a593344285ed0587d36
Ryan Searls <Ryan.searls@gmail.com>
2021-12-18 12:48:51 -0600

    leap year

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -600,16 +600,32 @@
 
 #=====================================================
 
-Given a year, report if it is a leap year.
+# Given a year, report if it is a leap year.
 
-The tricky thing here is that a leap year in the Gregorian calendar occurs:
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
 
-on every year that is evenly divisible by 4
-except every year that is evenly divisible by 100
-unless the year is also evenly divisible by 400
-For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
 
-If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+# If your language provides a method in the standard library that does this look-up, pretend it doesnt exist and implement it yourself.
+
+def leap_year?(number)
+  if number % 4 == 0 && number % 100 != 0 || number % 400 == 0
+    return true
+  else 
+    return false 
+  end 
+end 
+
+
+p leap_year?(1996)
+p leap_year?(2000)
+p leap_year?(1900)
+
+
+
   
 
 
