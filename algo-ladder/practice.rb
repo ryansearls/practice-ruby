e8b72ca0828262fcf26733acbc5e042bb9d0f27f
Ryan Searls <Ryan.searls@gmail.com>
2021-12-20 19:19:07 -0600

    dna to rna

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -804,6 +804,39 @@
 #=======================================================
 
 
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides. Here were representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, heres a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+
+
+def dna(string)
+  rna = {"G" => "C", "C" => "G", "T" => "A", "A" => "U"}
+  new_string = ""
+  index = 0
+  while index < string.length
+    new_string << rna[string[index]]
+    index += 1
+  end 
+  new_string  
+
+end 
+
+
+p dna('ACGTGGTCTTAA')
+
+
 
 
 
