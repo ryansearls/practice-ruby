36d5bc837ca716f57a657f161bb4db63f37f71ed
Ryan Searls <Ryan.searls@gmail.com>
2022-01-07 14:16:14 -0600

    less than 100

--- algo-ladder/practice.rb
+++ algo-ladder/practice.rb
@@ -853,6 +853,13 @@ end
 
 p reverse_string("Madam, I'm Adam")
 
+#==========================================================
+
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
 
 
 
