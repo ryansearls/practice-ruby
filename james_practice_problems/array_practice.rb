e9a306ba183c49439b213f1c93b2bfb84b26544c
Ryan Searls <Ryan.searls@gmail.com>
2022-02-01 13:45:49 -0700

    skip it

--- james_practice_problems/array_practice.rb
+++ james_practice_problems/array_practice.rb
@@ -120,19 +120,49 @@
 # Output: [5, 4, 3, 2, 1]
 
 
-def reverse_array(array)
-  new_array = []
-  index = array.length - 1
-  while index >= 0
-    new_array << array[index]
-    index -= 1
-  end 
-  new_array  
+# def reverse_array(array)
+#   new_array = []
+#   index = array.length - 1
+#   while index >= 0
+#     new_array << array[index]
+#     index -= 1
+#   end 
+#   new_array  
 
-end 
+# end 
 
 
-p reverse_array([1, 2, 3, 4, 5])
+# p reverse_array([1, 2, 3, 4, 5])
 
 #=======================================================
+
+# Given an array of numbers, write a function that returns a new array in which only select numbers from the original array are included, based on the following details:
+
+# The new array should always start with the first number from the original array. The next number that should be included depends on what the first number is. The first number dictates how many spaces to the right the computer should move to pick the next number. For example, if the first number is 2, then the next number that the computer should select would be two spaces to the right. This number gets added to the new array. If this next number happens to be a 4, then the next number after that is the one four spaces to the right. And so on and so forth until the end of the array is reached.
+
+# Input:
+# [2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2]
+
+# Output:
+# [2, 3, 1, 2, 2, 1, 5, 2, 2]
+
+
+
+# def skip_it(array)
+#   new_array = []
+#   index = 0
+#   while index < array.length
+#     new_array << array[index]
+#     index += array[index]
+#   end 
+#   new_array  
+
+# end 
+
+# p skip_it([2, 1, 3, 2, 5, 1, 2, 6, 2, 7, 1, 5, 6, 3, 2, 6, 2, 1, 2])
+
+#==============================================================
+
+
+
 
