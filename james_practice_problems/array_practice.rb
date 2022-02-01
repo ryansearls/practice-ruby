a0a18ecebfe2555bebef3cb125f879fed8219a30
Ryan Searls <Ryan.searls@gmail.com>
2022-02-01 12:28:04 -0700

    less than 100 array

--- james_practice_problems/array_practice.rb
+++ james_practice_problems/array_practice.rb
@@ -8,18 +8,40 @@
 # Explanation: (1 + 2 + 3 + 4) = 10
 
 
-def sum(array)
-  sum = 0
-  index = 0
-  while index < array.length
-    sum += array[index]
-    index += 1
-  end 
-  sum   
+# def sum(array)
+#   sum = 0
+#   index = 0
+#   while index < array.length
+#     sum += array[index]
+#     index += 1
+#   end 
+#   sum   
 
-end 
+# end 
 
-p sum([1, 2, 3, 4])
+# p sum([1, 2, 3, 4])
 
 #======================================================
 
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+# def less_than_100(array)
+#   new_array = []
+#   index = 0
+#   while index < array.length
+#     if array[index] < 100
+#       new_array << array[index]
+#     end 
+#     index += 1
+#   end     
+#   new_array
+
+# end 
+
+
+# p less_than_100([99, 101, 88, 4, 2000, 50])
+
+#=====================================================================
