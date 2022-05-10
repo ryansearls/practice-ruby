4843d2b28397b37fd4f27e420975386542b5826e
Ryan Searls <Ryan.searls@gmail.com>
2022-05-09 20:32:50 -0600

    algo practice

---
+++ algo-ladder/practice2.rb
@@ -0,0 +1,95 @@
+# Write a function that returns the sum of all numbers in a given array.
+
+# NOTE: Do not use any built-in language functions that do this automatically for you.
+
+# Input: [1, 2, 3, 4]
+# Output: 10
+
+# Explanation: (1 + 2 + 3 + 4) = 10
+
+# def sum(array)
+#   sum = 0
+#   array.each do |number|
+#     sum += number
+#   end 
+#   sum
+# end 
+
+# p sum([1,2,3,4])
+
+# def sum(array)
+#   sum = 0
+#   index = 0
+#   while index < array.length
+#     sum += array[index]
+#     index += 1
+#   end 
+#   sum  
+# end 
+
+# p sum([1,2,3,4])
+
+# ---------------------------------------
+
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+
+# def less_than_100(array)
+#   new_array = []
+#   array.each do |number|
+#     if number < 100
+#       new_array << number
+#     end 
+#   end 
+#   new_array  
+# end 
+
+
+# p less_than_100([99, 101, 88, 4, 2000, 50])
+
+
+# ---------------------------------------------
+
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+# Input: [4, 2, 5, 99, -4]
+# Output: [8, 4, 10, 198, -8]
+
+# def double(array)
+#   doubled_nums = []
+#   array.each do |number|
+#     doubled_nums << number * 2
+#   end 
+#   doubled_nums
+
+# end 
+
+# p double([4, 2, 5, 99, -4])
+
+# --------------------------------------------------
+
+# Write a function that returns the greatest value from an array of numbers.
+
+# Input: [5, 17, -4, 20, 12]
+# Output: 20
+
+
+def greatest_number(array)
+  max = array[0]
+  index = 1
+  while index < array.length
+    if max < array[index]
+      max = array[index]
+    end 
+    index += 1  
+  end 
+  max
+end 
+
+
+p greatest_number([5, 17, -4, 20, 12])
+
+
--- skills-sheet/nested_array.rb
+++ skills-sheet/nested_array.rb
@@ -27,20 +27,35 @@
 # 2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
 
-def merge_array(array1, array2)
-  new_pairs = []
-  index1 = 0
-  while index1 < array1.length
-    index2 = 0
-    while index2 < array2.length
-      new_pairs << array1[index1] + array2[index2]
-      index2 += 1
-    end 
-    index1 += 1
-  end 
-  new_pairs    
+# def merge_array(array1, array2)
+#   new_pairs = []
+#   index1 = 0
+#   while index1 < array1.length
+#     index2 = 0
+#     while index2 < array2.length
+#       new_pairs << array1[index1] + array2[index2]
+#       index2 += 1
+#     end 
+#     index1 += 1
+#   end 
+#   new_pairs    
+
+# end 
+
+
+# p merge_array(["a", "b", "c"], ["d", "e", "f", "g"])
+
+#-----------------------------------------------------
+
+3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+#     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+def one_array(array)
+  
+
 
 end 
 
 
-p merge_array(["a", "b", "c"], ["d", "e", "f", "g"])
+p one_array(["a", "b", "c", "d"])
+
