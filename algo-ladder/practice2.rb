bdb2d270b33f4434d03e9119db51ed00787f1fce
Ryan Searls <Ryan.searls@gmail.com>
2022-05-10 13:59:45 -0600

    show me the money

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -184,18 +184,34 @@
 # p reverse_string('abcde')
 
 
-# def reverse(string)
-#   index = string.length - 1
-#   new_string = ''
-#   while index >= 0
-#     new_string << string[index]
-#     index -= 1
-#   end 
-#   new_string 
+# ----------------------------------------------------------
 
+
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
+
+
+# def money(string)
+#   index = 0
+#   while index < string.length
+#     if string[index] == '$'
+#       return true
+#     end
+#     index += 1
+#   end 
+#   return false     
 # end 
 
 
-# p reverse("abcde")
+# p money("i hate $ but i love money i know i know im crazy")
+# p money("abcdefghijklmnopqrstuvwxyz")
 
-----------------------------------------------------------
+# -----------------------------------------------------------------
+
+
+
