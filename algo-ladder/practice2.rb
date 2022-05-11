2f55479bf609d6bc17790f8fc02c3bd026336af8
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 15:08:19 -0600

    leap year

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -415,7 +415,37 @@
 
 # p fibonacci(9)
 
--------------------------------------------------------
+# -------------------------------------------------------
+
+# Given a year, report if it is a leap year.
+
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+# def leap?(year)
+#   if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
+#    return true
+#   else 
+#     return false
+#   end     
+# end 
+
+
+# p leap?(1995)
+# p leap?(1900)
+# p leap?(1600)
+# p leap?(1928)
+# p leap?(1916)
+
+# ------------------------------------------------------------------
+
+
 
 
 
