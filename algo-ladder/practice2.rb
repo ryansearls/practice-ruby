6dffe24151b59741488cb53aabc85d21b8de2678
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 12:02:31 -0600

    fibonacci

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -375,25 +375,47 @@
 # If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
 
 
-def fizzbuzz(number)
-  index = 1
-  while index <= number
-    if index % 3 == 0 && index % 5 == 0
-      puts "FIZZBUZZ"
-    elsif index % 3 == 0 
-      puts "FIZZ"
-    elsif index % 5 == 0 
-      puts "BUZZ"
-    elsif !(index % 3 == 0 || index % 5 == 0)
-      puts index 
-    end       
-    index += 1
-  end   
-end 
+# def fizzbuzz(number)
+#   index = 1
+#   while index <= number
+#     if index % 3 == 0 && index % 5 == 0
+#       puts "FIZZBUZZ"
+#     elsif index % 3 == 0 
+#       puts "FIZZ"
+#     elsif index % 5 == 0 
+#       puts "BUZZ"
+#     elsif !(index % 3 == 0 || index % 5 == 0)
+#       puts index 
+#     end       
+#     index += 1
+#   end   
+# end 
 
-fizzbuzz(17)
+# fizzbuzz(17)
 
 # --------------------------------------------------------------
+
+# Write a function that gives the Nth number of the Fibonacci Sequence. The Fibonacci sequence begins with 0 and 1, and every number thereafter is the sum of the previous two numbers. So the sequence goes like this:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, and so on until infinity...
+
+# Input: 9
+# Output: 21 (as this is the 9th number of the Fibonacci Sequence)
+
+# def fibonacci(number)
+#     num1, num2 = [0,1]
+ 
+#   (number - 1).times do 
+#     num1, num2 = num2, num1+num2
+#   end 
+#   num1
+
+# end 
+
+
+# p fibonacci(9)
+
+-------------------------------------------------------
 
 
 
