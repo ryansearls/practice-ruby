5f06957cff2e43126218466c28918ca2e3163f88
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 15:40:15 -0600

    collatz conjecture

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -449,25 +449,68 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def multiples_3_and_5(number)
-  sum = 0
-  index = 0
-  while index < number
-    if index % 3 == 0
-      sum += index
-    elsif index % 5 == 0
-      sum += index
-    end 
-    index += 1
+# def multiples_3_and_5(number)
+#   sum = 0
+#   index = 0
+#   while index < number
+#     if index % 3 == 0
+#       sum += index
+#     elsif index % 5 == 0
+#       sum += index
+#     end 
+#     index += 1
+#   end 
+#   sum       
+
+
+
+# end 
+
+
+# p multiples_3_and_5(1000)
+
+# ---------------------------------------------------
+
+# The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+# Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+
+# Given a number n, return the number of steps required to reach 1.
+
+# Examples
+# Starting with n = 12, the steps would be as follows:
+
+# 12
+# 6
+# 3
+# 10
+# 5
+# 16
+# 8
+# 4
+# 2
+# 1
+
+# Resulting in 9 steps. So for input n = 12, the return value would be 9.
+
+
+def collatz(number)
+  steps = 0
+  while number > 1
+    if number % 2 == 0
+      number = number / 2 
+    else
+      number = (number * 3) + 1
+    end
+    steps += 1
   end 
-  sum       
-
-
-
+  steps
 end 
 
 
-p multiples_3_and_5(1000)
+p collatz(12)
+
+# -------------------------------------------------
 
 
 
