9bd34d2262d9a9e55bc269f5696b1a5d2ff472f0
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 15:17:37 -0600

    multiples of 3 or 5

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -445,6 +445,30 @@
 
 # ------------------------------------------------------------------
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiples_3_and_5(number)
+  sum = 0
+  index = 0
+  while index < number
+    if index % 3 == 0
+      sum += index
+    elsif index % 5 == 0
+      sum += index
+    end 
+    index += 1
+  end 
+  sum       
+
+
+
+end 
+
+
+p multiples_3_and_5(1000)
+
 
 
 
