5ddcb5656bee7ada4c9e35cf3602026e7bc27480
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 11:57:16 -0600

    fizzbuzz

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -368,7 +368,32 @@
 
 # ------------------------------------------------------
 
+# Write a function that prints out every number from 1 to N, with the following exceptions:
 
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+def fizzbuzz(number)
+  index = 1
+  while index <= number
+    if index % 3 == 0 && index % 5 == 0
+      puts "FIZZBUZZ"
+    elsif index % 3 == 0 
+      puts "FIZZ"
+    elsif index % 5 == 0 
+      puts "BUZZ"
+    elsif !(index % 3 == 0 || index % 5 == 0)
+      puts index 
+    end       
+    index += 1
+  end   
+end 
+
+fizzbuzz(17)
+
+# --------------------------------------------------------------
 
 
 
