d600b23692f98e58764561b8d8b8df99eb528e11
Ryan Searls <Ryan.searls@gmail.com>
2022-05-11 21:36:15 -0600

    social media likes

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -550,16 +550,58 @@
 # Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
 
 
-def whole_menu(hash)
-  total = 0
-  hash.each do |k,v|
-    total += v
-  end 
-  total  
+# def whole_menu(hash)
+#   total = 0
+#   hash.each do |k,v|
+#     total += v
+#   end 
+#   total  
 
-end 
+# end 
 
-p whole_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+# p whole_menu({"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
+
+
+# --------------------------------------------
+
+
+# Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+# Input: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+# ]
+
+# Output: [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+
+
+# def media_posts(array)
+#   over_1000 = []
+#   index = 0
+#   while index < array.length
+#     if array[index][:likes] > 1000
+#       over_1000 << array[index]
+#     end 
+#     index += 1
+#   end 
+#   over_1000    
+
+# end 
+
+
+# p media_posts([
+#   {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+#   {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+#   {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+#   {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+#   ])
+
+
+----------------------------------------------------
 
 
 
