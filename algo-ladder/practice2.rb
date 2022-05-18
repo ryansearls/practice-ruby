8edac196de74041719aea0a9538dd382f6a864f4
Ryan Searls <Ryan.searls@gmail.com>
2022-05-17 20:08:58 -0600

    array mesh 2

--- algo-ladder/practice2.rb
+++ algo-ladder/practice2.rb
@@ -510,37 +510,8 @@
 
 # p collatz(12)
 
-# -------------------------------------------------
-
-# Given two arrays of strings, return a new string that contains every combination of a string from the first array concatenated with a string from the second array.
-
-Input: ["a", "b", "c"], ["d", "e", "f", "g"]
-Output: ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"]
-
-def array_mesh_1(string1, string2)
-  new_array = []
-  index1 = 0
-  while index1 < string1.length
-    index2 = 0
-    while index2 < string2.length
-      new_array << string1[index1] + string2[index2]
-      index2 += 1
-    end 
-    index1 += 1
-  end 
-  new_array      
 
 
-end 
-
-
-p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
-
-
-
-
-
-# ----------------------------------------------------
 
 
 # Given a string, find the most commonly occurring letter.
@@ -736,9 +707,8 @@ p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
 #   array.each do |hash|
 #     hash[:submitted_by] == user_hash[hash[:submitted_by]]
 #     updated_list << hash
-#   end 
+    # end
 #   updated_list  
-
 # end 
 
 
