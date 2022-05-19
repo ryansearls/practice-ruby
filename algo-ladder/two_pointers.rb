10e11c990e3ac2e64d1d4c93202fd8413157ca81
Ryan Searls <Ryan.searls@gmail.com>
2022-05-19 16:37:23 -0600

    largest product

--- algo-ladder/two_pointers.rb
+++ algo-ladder/two_pointers.rb
@@ -228,10 +228,3 @@ end
 
 
 array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
-
-
-
-Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
-
-Input: ["a", "b", "c", "d"]
-Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
--- algo-ladder/two_pointers2.rb
+++ algo-ladder/two_pointers2.rb
@@ -10,19 +10,41 @@
 #   while index1 < string1.length
 #     index2 = 0
 #     while index2 < string2.length
-#       new_array = string1[index1] + string2[index2]
+#       new_array << string1[index1] + string2[index2]
 #       index2 += 1
 #     end 
 #     index1 += 1
 #   end 
-#   new_array    
+#   new_array
 
 # end 
 
 
 # p array_mesh_1(["a", "b", "c"], ["d", "e", "f", "g"])
 
-
 # ---------------------------------------------
 
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
 
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+
+def array_mesh_2(array)
+  new_array = []
+  index1 = 0
+  while index1 < array.length
+    index2 = 0
+    while index2 < array.length
+      if array[index1] != array[index2]
+        new_array << array[index1] + array[index2]
+      end 
+      index2 += 1
+    end 
+    index1 += 1
+  end   
+  new_array
+end 
+
+
+p array_mesh_2(["a", "b", "c", "d"])
