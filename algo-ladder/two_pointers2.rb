424aee18c8cba60ea05a243371441ebc1e024a26
Ryan Searls <Ryan.searls@gmail.com>
2022-06-07 12:39:20 -0600

    coolio

--- algo-ladder/two_pointers2.rb
+++ algo-ladder/two_pointers2.rb
@@ -144,5 +144,38 @@
 
 #------------------------------------------------------------
 
+# Given an array of numbers, return true if the array is a "100 Coolio Array", or false if it is not.
+
+# A "100 Coolio Array" meets the following criteria:
+
+# Its first and last numbers add up to 100,
+# Its second and second-to-last numbers add up to 100,
+# Its third and third-to-last numbers add up to 100,
+# and so on and so forth.
+
+# Here are examples of 100 Coolio Arrays:
+
+# [1, 2, 3, 97, 98, 99]
+# [90, 20, 70, 100, 30, 80, 10]
+
+
+def coolio?(array)
+  index1 = 0
+  index2 = array.length - 1
+  while index1 < array.length / 2
+    if array[index1] + array[index2] != 100 
+      return false
+    end
+    index1 += 1
+    index2 -= 1
+  end 
+  return true
+end 
+
+
+p coolio?([1, 2, 3, 97, 98, 99]) #true
+p coolio?([90, 20, 70, 100, 30, 80, 10]) #true
+p coolio?([90, 25, 70, 100, 30, 80, 10]) #false
+
 
 
