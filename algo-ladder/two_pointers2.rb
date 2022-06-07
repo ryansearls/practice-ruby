3f894b37f49664183e5648e3c4941068d02b5775
Ryan Searls <Ryan.searls@gmail.com>
2022-06-07 11:44:41 -0600

    Two Sum I

--- algo-ladder/two_pointers2.rb
+++ algo-ladder/two_pointers2.rb
@@ -57,20 +57,54 @@
 # Output: 63 (-9 * -7)
 
 
+# def largest_product(array)
+#   product = 0
+#   array.each_with_index do |x1, indx1|
+#     array.each_with_index do |x2, indx2|
+#       next if indx1 == indx2
+#       if product < (x1 * x2)
+#         product = (x1 * x2)
+#       end 
+#     end 
+#   end 
+#   product      
 
-def largest_product(array)
-  product = 0
-  array.each_with_index do |x1, indx1|
-    array.each_with_index do |x2, indx2|
-      next if indx1 == indx2
-      if product < (x1 * x2)
-        product = (x1 * x2)
+# end 
+
+
+# p largest_product([5, -2, 1, -9, -7, 2, 6])
+
+#----------------------------------------------------------
+
+# Given an array of numbers, return a new array containing just two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+
+# Specifically use nested loops to solve this exercise even though there are other approaches as well.
+
+# Input: [2, 5, 3, 1, 0, 7, 11]
+# Output: [3, 7]
+
+# Input: [1, 2, 3, 4, 5]
+# Output: false (While 1, 2, 3, and 4 altogether add up to 10, were seeking just one pair of numbers.)
+
+def equals_10(array)
+  new_array = []
+  index1 = 0
+  while index1 < array.length
+    index2 = index1 + 1
+    while index2 <= array.length - 1
+      if array[index1] + array[index2] == 10
+        new_array << array[index1]
+        new_array << array[index2]
+        return new_array
       end 
+      index2 += 1
     end 
+    index1 += 1
   end 
-  product      
+  return false      
 
 end 
 
+p equals_10([2, 5, 3, 1, 0, 7, 11])
 
-p largest_product([5, -2, 1, -9, -7, 2, 6])
+#--------------------------------------------------------------
