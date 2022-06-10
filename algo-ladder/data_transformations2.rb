91875abd7fd4a39bb6b47c5c952be0433bbd9264
Ryan Searls <Ryan.searls@gmail.com>
2022-06-10 13:57:09 -0600

    flip hash

--- algo-ladder/data_transformations2.rb
+++ algo-ladder/data_transformations2.rb
@@ -33,14 +33,33 @@
 # Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
 # Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
 
-def flatten_hash(hash)
-  array = []
+# def flatten_hash(hash)
+#   array = []
+#   hash.each do |k, v|
+#     array << k
+#     array << v
+#   end 
+#   array
+# end 
+
+
+# p flatten_hash({'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4})
+
+#-------------------------------------------------------------------------
+
+# Given a hash, create a new hash that has the keys and values switched.
+
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+def flip_hash(hash)
+  new_hash = {}
   hash.each do |k, v|
-    array << k
-    array << v
+    new_hash[v] = k
   end 
-  array
+  new_hash  
 end 
 
+p flip_hash({"a" => 1, "b" => 2, "c" => 3})
 
-p flatten_hash({'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4})
+#---------------------------------------------------------------------
