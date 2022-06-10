3c3d31b3e3a221de5c48ff1d785e171a6155c8ca
Ryan Searls <Ryan.searls@gmail.com>
2022-06-10 13:31:21 -0600

    total sum

---
+++ algo-ladder/basic_array2.rb
@@ -0,0 +1,20 @@
+# Write a function that returns the sum of all numbers in a given array.
+
+# NOTE: Do not use any built-in language functions that do this automatically for you.
+
+# Input: [1, 2, 3, 4]
+# Output: 10
+
+# Explanation: (1 + 2 + 3 + 4) = 10
+
+def sum(array)
+  total = 0
+  array.each do |number|
+    total += number
+  end 
+  total  
+
+
+end 
+
+p sum([1, 2, 3, 4])
