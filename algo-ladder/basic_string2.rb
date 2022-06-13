c57762caff92acef3a6de3f5a7fe186a5c533e86
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 16:12:45 -0600

    Hamming

--- algo-ladder/basic_string2.rb
+++ algo-ladder/basic_string2.rb
@@ -107,3 +107,31 @@
 # p palindrome?('racecaaaar')
 
 #----------------------------------------------------------------
+
+# Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+# Input: "ABCDEFG", "ABCXEOG"
+# Output: 2
+
+# Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+# Input: "ABCDEFG", "ABCDEFG",
+# Output: 0
+
+
+# def hamming(string1, string2)
+#   output = 0
+#   index = 0
+#   while index < string1.length
+#     if string1[index] != string2[index]
+#       output += 1
+#     end 
+#     index += 1
+#   end 
+#   output    
+# end 
+
+
+# p hamming("ABCDEFG", "ABCXEOG")
+
+#-----------------------------------------------------------------
