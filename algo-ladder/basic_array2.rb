c4830868454804fad9c9ea64ebaab23658faee58
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 13:30:19 -0600

    less than 100 array

--- algo-ladder/basic_array2.rb
+++ algo-ladder/basic_array2.rb
@@ -7,15 +7,33 @@
 
 # Explanation: (1 + 2 + 3 + 4) = 10
 
-def sum(array)
-  total = 0
-  array.each do |number|
-    total += number
-  end 
-  total  
-end 
+# def sum(array)
+#   total = 0
+#   array.each do |number|
+#     total += number
+#   end 
+#   total  
+# end 
 
-p sum([1, 2, 3, 4])
+# p sum([1, 2, 3, 4])
 
 #-------------------------------------------------------------------------
 
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+# def less_than_100(array)
+#   new_array = []
+#   array.each do |num|
+#     if num < 100
+#       new_array << num
+#     end 
+#   end 
+#   new_array
+# end 
+
+# p less_than_100([99, 101, 88, 4, 2000, 50])
+
+#-----------------------------------------------------------
