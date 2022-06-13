3076c14c999716277c54ba47f54167675e8a9141
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 15:51:11 -0600

    palindrome

--- algo-ladder/basic_string2.rb
+++ algo-ladder/basic_string2.rb
@@ -81,3 +81,29 @@
 # p first_duplicate('abcdefghhijkkloooop')
 
 #------------------------------------------------------------------
+
+# Given a string, write a function that returns true if it is a palindrome, and false if it is not. (A palindrome is a word that reads the same both forward and backward.)
+
+# Input: “racecar”
+# Output: true
+
+# Input: “baloney”
+# Output: false
+
+# def palindrome?(string)
+#   index1 = 0
+#   index2 = string.length - 1
+#   while index1 < string.length / 2
+#     if string[index1] != string[index2]
+#       return false
+#     end 
+#     index1 += 1
+#     index2 -= 1
+#   end 
+#   return true     
+# end 
+
+# p palindrome?('racecar')
+# p palindrome?('racecaaaar')
+
+#----------------------------------------------------------------
