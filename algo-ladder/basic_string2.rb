81d0adeaf003fd8455e8198929529c9a39e2f0ca
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 15:27:53 -0600

    Alternate Capitals

--- algo-ladder/basic_string2.rb
+++ algo-ladder/basic_string2.rb
@@ -26,16 +26,39 @@
 # Output: false
 
 
-def show_me_the_money(string)
-  string.each_char do |char|
-    if char == "$"
-      return true 
-    end
-  end
-  return false 
-end
+# def show_me_the_money(string)
+#   string.each_char do |char|
+#     if char == "$"
+#       return true 
+#     end
+#   end
+#   return false 
+# end
 
-p show_me_the_money("i hate $ but i love money i know i know im crazy")
-p show_me_the_money("iabcdefghijklmnopqrstuvwxyz")
+# p show_me_the_money("i hate $ but i love money i know i know im crazy")
+# p show_me_the_money("iabcdefghijklmnopqrstuvwxyz")
 
 #--------------------------------------------------------------
+
+# Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
+
+#   Input: “hello, how are your porcupines today?”
+#   Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+# def every_other_capitalized(string)
+#   new_string = ''
+#   index1 = 0
+#   index2 = 1
+#   while index1 < string.length
+#     new_string << string[index1]
+#     new_string << string[index2].to_s.upcase
+#     index1 += 2
+#     index2 += 2
+#   end 
+#   new_string  
+
+# end 
+
+# p every_other_capitalized('hello, how are your porcupines today?')
+
+#-------------------------------------------------------------------
