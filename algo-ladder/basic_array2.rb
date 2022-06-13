88d8c18865332083c428511e8bd3f879c5c77098
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 13:42:11 -0600

    doubled array

--- algo-ladder/basic_array2.rb
+++ algo-ladder/basic_array2.rb
@@ -37,3 +37,18 @@
 # p less_than_100([99, 101, 88, 4, 2000, 50])
 
 #-----------------------------------------------------------
+
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+# Input: [4, 2, 5, 99, -4]
+# Output: [8, 4, 10, 198, -8]
+
+def double(array)
+  new_array = []
+  array.each do |number|
+    new_array << number * 2
+  end 
+  new_array
+end 
+
+p double([4, 2, 5, 99, -4])
