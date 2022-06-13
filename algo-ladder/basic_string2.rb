ea555fe0a151cf9ef05fac3ea766a7689ab30432
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 15:03:47 -0600

    Reverse String

---
+++ algo-ladder/basic_string2.rb
@@ -0,0 +1,18 @@
+# Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+def reverse_string(string)
+  new_string = ''
+  index = string.length - 1
+  while index >= 0
+    new_string << string[index]
+    index -= 1
+  end 
+  new_string  
+end 
+
+p reverse_string('abcde')
+
+#-----------------------------------------------------------
