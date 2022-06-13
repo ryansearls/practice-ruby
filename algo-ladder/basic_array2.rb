f35147225e6a49843e5b7537437a340c816b5e2e
Ryan Searls <Ryan.searls@gmail.com>
2022-06-13 14:08:15 -0600

    reverse array

--- algo-ladder/basic_array2.rb
+++ algo-ladder/basic_array2.rb
@@ -98,3 +98,20 @@
 
 #---------------------------------------------------------------
 
+# Given an array, write a function that returns an array that contains the original array’s values in reverse.
+
+# Input: [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+def reverse_array(array)
+  new_array = []
+  index = array.length - 1
+  while index >= 0
+    new_array << array[index]
+    index -= 1
+  end 
+  new_array  
+end 
+
+
+p reverse_array([1, 2, 3, 4, 5])
