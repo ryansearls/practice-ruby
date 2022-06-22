961730b71519da41af39d43a2010bf782a72168f
Ryan Searls <Ryan.searls@gmail.com>
2022-06-21 18:19:29 -0600

    Insertion Sort

--- algo-ladder/bubble_sort.rb
+++ algo-ladder/bubble_sort.rb
@@ -20,3 +20,23 @@
 # p bubble_sort([50, 17, 56, 6, 83, 261, -4, 45, 34, 8, 1])
 
 #------------------------------------------------------------------------
+
+def insertion_sort(array)
+
+  array.each_with_index do |number, index|
+    i = index - 1 
+          
+    while i>=0
+      break if number >= array[i]
+      array[i+1] = array[i]
+      i -= 1
+    end 
+  
+    array[i+1] = number
+  
+  end 
+end
+
+p insertion_sort([1,3,4,2,7,6,8])
+
+#-------------------------------------------------------------------------
