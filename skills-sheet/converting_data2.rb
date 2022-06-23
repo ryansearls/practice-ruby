cefeb993550f087500144d51037bc42d5d2b9397
Ryan Searls <Ryan.searls@gmail.com>
2022-06-23 17:39:49 -0600

    Hash to Array of Hashes

--- skills-sheet/converting_data2.rb
+++ skills-sheet/converting_data2.rb
@@ -70,3 +70,22 @@
 # p hash_to_array({"chair" => 100, "book" => 14})
 
 #-----------------------------------------------------------------------
+
+#  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+#     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+def hash_to_array_of_hashes(hash)
+  array = []
+  hash.each do |id, person|
+    person[:id] = id
+    array << person
+  end 
+  array  
+end 
+
+
+p hash_to_array_of_hashes({321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}})
+
+#--------------------------------------------------------------------------
+
+
