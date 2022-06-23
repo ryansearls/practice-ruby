458bb406d9a338a4d1b06bf69939d7c9887db195
Ryan Searls <Ryan.searls@gmail.com>
2022-06-23 17:31:15 -0600

    Hash to Array

--- skills-sheet/converting_data2.rb
+++ skills-sheet/converting_data2.rb
@@ -37,19 +37,36 @@
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
 
-def string_to_hash(string)
-  hash = {}
-  index = 0
-  while index < string.length
-    if hash[string[index]]
-      hash[string[index]] += 1
-    else 
-      hash[string[index]] = 1
-    end 
-    index += 1 
-  end 
-  hash
-end 
+# def string_to_hash(string)
+#   hash = {}
+#   index = 0
+#   while index < string.length
+#     if hash[string[index]]
+#       hash[string[index]] += 1
+#     else 
+#       hash[string[index]] = 1
+#     end 
+#     index += 1 
+#   end 
+#   hash
+# end 
+
+# p string_to_hash("bookkeeper")
+
+#----------------------------------------------------------------------
+
+#  4. Convert a hash into an array of arrays.
+#     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+# def hash_to_array(hash)
+#   new_array = []
+#   hash.each do |k, v|
+#     new_array << [k, v]
+#   end 
+#   new_array
+# end 
 
 
-p string_to_hash("bookkeeper")
+# p hash_to_array({"chair" => 100, "book" => 14})
+
+#-----------------------------------------------------------------------
