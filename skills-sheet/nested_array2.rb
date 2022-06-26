44c0ca96eae9baf00c71bfea29c57e923d08efdb
Ryan Searls <Ryan.searls@gmail.com>
2022-06-25 21:46:39 -0600

    Arrays to Hash

---
+++ skills-sheet/nested_array2.rb
@@ -0,0 +1,18 @@
+#  1. Convert an array of arrays into a hash.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
+
+# def arrays_to_hash(array)
+#   hash = {}
+#   index = 0
+#   while index < array.length
+#     key = array[index][0]
+#     value = array[index][1]
+#     hash[key] = value
+#     index += 1
+#   end 
+#   hash  
+# end 
+
+# p arrays_to_hash([[1, 3], [8, 9], [2, 16]])
+
+#-----------------------------------------------------------------------------
