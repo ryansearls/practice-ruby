62187cd32138e9c705f41871825c6b61b568abf3
Ryan Searls <Ryan.searls@gmail.com>
2022-06-25 20:59:15 -0600

    Array of Hashes to Hash of Arrays

--- skills-sheet/converting_data2.rb
+++ skills-sheet/converting_data2.rb
@@ -146,3 +146,8 @@
 # p mash_hashes({"chair" => 75, "book" => 15}, [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}])
 
 #-----------------------------------------------------------------------
+
+#  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
+#     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+
+
