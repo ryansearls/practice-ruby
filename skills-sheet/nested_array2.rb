8728553fccdbd972c5b6e1acedf1f6b9f4faaaa2
Ryan Searls <Ryan.searls@gmail.com>
2022-06-26 21:38:42 -0600

    Two Arrays to One

--- skills-sheet/nested_array2.rb
+++ skills-sheet/nested_array2.rb
@@ -15,3 +15,24 @@
 # p flat_hash([[1, 3], [8, 9], [2, 16]])
 
 #--------------------------------------------------------------------------
+
+#  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+#     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
+
+# def two_array(array1, array2)
+#   new_array = []
+#   index1 = 0
+#   while index1 < array1.length
+#     index2 = 0
+#     while index2 < array2.length
+#       new_array << array1[index1] + array2[index2]
+#       index2 += 1
+#     end 
+#     index1 += 1
+#   end 
+#   new_array    
+# end 
+
+# p two_array(["a", "b", "c"], ["d", "e", "f", "g"])
+
+#-------------------------------------------------------------------------
