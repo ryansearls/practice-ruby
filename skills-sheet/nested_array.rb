c18b2a03335a437a3ea457b3818a915495e1b659
Ryan Searls <Ryan.searls@gmail.com>
2022-06-29 12:08:18 -0600

    Letter Combo

--- skills-sheet/nested_array.rb
+++ skills-sheet/nested_array.rb
@@ -45,4 +45,24 @@
 # 3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 # #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
+def letter_combo(array)
+  letter_combinations = []
+  index1 = 0
+  while index1 < array.length
+    index2 = 0
+    while index2 < array.length
+      if index1 != index2
+        letter_combinations << array[index1] + array[index2]
+      end
+      index2 += 1
+    end
+    index1 += 1
+  end
+  letter_combinations
+end   
+
+p letter_combo(["a", "b", "c", "d"])
+
+#---------------------------------------------------------
+
 
