7fa21616fdae56d4746111429e06d0bcee3b7fd3
Ryan Searls <Ryan.searls@gmail.com>
2022-06-29 19:10:35 -0600

    Print Hash Keys

---
+++ skills-sheet/hash_practice2.rb
@@ -0,0 +1,9 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = {first_name: "Ada", last_name: "Lovelace", email: "ada.lovelace@email.com" }
+
+puts person[:first_name]
+puts person[:last_name]
+puts person[:email]
+
+#----------------------------------------------------------------------------------
