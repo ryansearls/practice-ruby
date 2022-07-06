1774499e9584cf193e4919694363e636740db09f
Ryan Searls <Ryan.searls@gmail.com>
2022-07-05 21:12:10 -0600

    Print Books Author

--- skills-sheet/hash_practice2.rb
+++ skills-sheet/hash_practice2.rb
@@ -39,3 +39,17 @@
 # p book[:language]
 
 #-------------------------------------------------------------------------------------
+
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+# books = [
+#   { title: "Reality is Broken", author: "Jane McGonigal" },
+#   { title: "Code", author: "Charles Petzlod" },
+#   { title: "The Humane Interface", author: "Jef Raskin" }
+# ]
+
+# p books[2][:author]
+
+#----------------------------------------------------------------------------------------
+
+
