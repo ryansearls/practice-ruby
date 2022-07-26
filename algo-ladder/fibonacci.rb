aaf8a9fe690efa63942dc1f92d622eee51b458c3
Ryan Searls <Ryan.searls@gmail.com>
2022-07-26 17:50:40 -0600

    leap year

--- algo-ladder/fibonacci.rb
+++ algo-ladder/fibonacci.rb
@@ -17,3 +17,29 @@
 # p fib(9)
 
 #--------------------------------------------------------------------------
+
+# Given a year, report if it is a leap year.
+
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+
+def leap_year(year)
+  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    return true
+  else 
+    return false
+  end   
+end 
+
+
+p leap_year(1997)
+p leap_year(1996)
+p leap_year(1900)
+p leap_year(1600)
+
+
