fe893c44a15494e657ce0f4255f1abdf3128ee07
Ryan Searls <Ryan.searls@gmail.com>
2022-08-11 17:19:05 -0600

    linear search

--- must-knows/linear_search.rb
+++ must-knows/linear_search.rb
@@ -1,11 +1,14 @@
-def linear_search(list, n)
+def linear_search(list, item)
   index = 0
   while index < list.size
-    return n if list[index] == n
+    if list[index] == item
+      return "#{item} was found at index #{index}"
+    end   
     index += 1
   end 
 
+  return "#{item} was not found in this array"
 
 end 
 
-p linear_search([1,3,4,5,6,7,9,11], 4)
+p linear_search([1,3,4,5,6,7,9,11], 11)
