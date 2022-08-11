c7b638c4b67705974788100421f7ec637176279d
Ryan Searls <Ryan.searls@gmail.com>
2022-08-11 16:00:01 -0600

    linear search

--- must-knows/insertion_sort.rb
+++ must-knows/insertion_sort.rb
@@ -1,17 +1,17 @@
-# def insertion_sort(array)
+def insertion_sort(array)
 
-#   array.each_with_index do |number, index|
-#     i = index - 1 
+  array.each_with_index do |number, index|
+    i = index - 1 
           
-#     while i>=0
-#       break if number >= array[i]
-#       array[i+1] = array[i]
-#       i -= 1
-#     end 
+    while i>=0
+      break if number >= array[i]
+      array[i+1] = array[i]
+      i -= 1
+    end 
   
-#     array[i+1] = number
+    array[i+1] = number
   
-#   end 
-# end
+  end 
+end
 
-# p insertion_sort([1,3,4,2,7,6,8])
+p insertion_sort([1,3,4,2,7,6,8])
