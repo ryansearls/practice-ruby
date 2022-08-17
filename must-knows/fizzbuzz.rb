6382afa696d52fe97c3770f161800b30c4340d80
Ryan Searls <Ryan.searls@gmail.com>
2022-08-16 18:42:39 -0600

    adds fizzbuzz

---
+++ must-knows/fizzbuzz.rb
@@ -0,0 +1,30 @@
+# Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+def fizzbuzz(n)
+  index = 1
+  while index <= n
+    if index % 3 == 0 && index % 5 == 0
+      puts "FIZZBUZZ"
+    elsif 
+      index % 5 == 0
+      puts "BUZZ"
+    elsif 
+      index % 3 == 0
+      puts "FIZZ"
+    else 
+      puts "#{index}"
+    end 
+    index += 1
+  end 
+end 
+
+
+p fizzbuzz(19)
+p fizzbuzz(7)
+p fizzbuzz(15)
+
