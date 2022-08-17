dd51fd2b0815f547520b7bca702ba6f6536d8fbe
Ryan Searls <Ryan.searls@gmail.com>
2022-08-17 12:44:31 -0600

    makes palindrome file

--- must-knows/reverse_string.rb
+++ must-knows/reverse_string.rb
@@ -4,16 +4,16 @@
 # Output: “edcba”
 
 
-def reverse_string(string)
-  index = string.length - 1
-  new_string = ''
-  while index >= 0
-    new_string << string[index]
-    index -= 1
-  end 
-  new_string
-end 
+# def reverse_string(string)
+#   index = string.length - 1
+#   new_string = ''
+#   while index >= 0
+#     new_string << string[index]
+#     index -= 1
+#   end 
+#   new_string
+# end 
 
 
-p reverse_string("edcba")
-p reverse_string("abcdefghij")
+# p reverse_string("edcba")
+# p reverse_string("abcdefghij")
