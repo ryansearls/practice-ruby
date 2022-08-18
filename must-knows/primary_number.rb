a7f0222782a30b284ed68f1432c32eb80df343a6
Ryan Searls <Ryan.searls@gmail.com>
2022-08-17 18:18:59 -0600

    prime numbers II

--- must-knows/primary_number.rb
+++ must-knows/primary_number.rb
@@ -1,12 +1,18 @@
-Write a function that returns whether a given number is a prime number.
+# Write a function that returns whether a given number is a prime number.
 
 def prime_number(number)
   if number < 2
     return false
   end 
 
-  
-
+  i = 2
+  while i < number
+    return false if number % i == 0
+    i += 1
+  end
+  true
 end 
 
 p prime_number(7)
+
+
